1053b80a13b3afe6234db7401d0cb505da0276a7
Felipe Cechin <ficechin@hotmail.com>
2022-07-07 15:13:54 -0300

    added new text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React and Deploy tests
         </a>
       </header>
     </div>
